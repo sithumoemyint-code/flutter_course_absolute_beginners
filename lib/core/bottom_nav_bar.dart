4d218f39ffb03274152948efdf4e74b31c5951d6
Si Thu Moe Myint <sithumoemyint@Sis-MacBook-Air.local>
2025-10-02 13:20:49 +0630

    bottom nav bar

---
+++ lib/core/bottom_nav_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyBottomNavBar extends StatefulWidget {
+  const MyBottomNavBar({super.key});
+
+  @override
+  State<MyBottomNavBar> createState() => _MyBottomNavBarState();
+}
+
+class _MyBottomNavBarState extends State<MyBottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My tickets")),
+      body: const Center(child: Text("Ticket info")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
