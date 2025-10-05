d0fcc41a1d06d4b8105837ac2e31f18a4524671c
Si Thu Moe Myint <sithumoemyint@Sis-MacBook-Air.local>
2025-10-05 09:27:12 +0630

    setup section 2

---
+++ lib/base/widgets/app_double_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppDoubleText extends StatefulWidget {
+  const AppDoubleText({super.key});
+
+  @override
+  State<AppDoubleText> createState() => _AppDoubleTextState();
+}
+
+class _AppDoubleTextState extends State<AppDoubleText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
