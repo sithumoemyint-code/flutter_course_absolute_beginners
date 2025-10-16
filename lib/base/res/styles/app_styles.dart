1f4f4e66cf7e02e82ce291d66c6d0d6a5e4c4adc
Si Thu Moe Myint <sithumoemyint@Sis-MacBook-Air.local>
2025-10-16 08:50:53 +0630

    search screen UI done

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -12,6 +12,7 @@ class AppStyles {
   static Color ticketTabColor = const Color(0xFFF4F6FD);
   static Color planeColor = const Color(0xFFBFC2DF);
   static Color findTicketColor = const Color(0xD91130CE);
+  static Color circleColor = const Color(0xFF1899999);
 
   static TextStyle textStyle = TextStyle(
     fontSize: 16,
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/base/res/media.dart' show AppMedia;
 import 'package:my_app/base/res/styles/app_styles.dart' show AppStyles;
 import 'package:my_app/base/widgets/app_double_text.dart' show AppDoubleText;
 import 'package:my_app/routes/routes.dart' show AppRoutes;
@@ -9,14 +8,14 @@ import 'package:my_app/screens/search/widgets/app_ticket_tabs.dart'
     show AppTicketTabs;
 import 'package:my_app/screens/search/widgets/find_tickets.dart'
     show FindTickets;
+import 'package:my_app/screens/search/widgets/ticket_promotion.dart'
+    show TicketPromotion;
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
       body: ListView(
@@ -45,84 +44,7 @@ class SearchScreen extends StatelessWidget {
             func: () => Navigator.pushNamed(context, AppRoutes.allTickets),
           ),
           const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 15,
-                ),
-                width: size.width * .42,
-                height: 405,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade200,
-                      blurRadius: 1,
-                      spreadRadius: 2,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(AppMedia.planeSit),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 12),
-                    Text(
-                      "20% discount on the early booking of this flight. Don't miss",
-                      style: AppStyles.headLineStyle3,
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: size.width * .44,
-                    height: 210,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF3A8888),
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Discount\nfor survey",
-                          style: AppStyles.headLineStyle2.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          "Take the survey about our services and get discount",
-                          style: AppStyles.headLineStyle2.copyWith(
-                            fontWeight: FontWeight.w500,
-
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          const TicketPromotion(),
         ],
       ),
     );
